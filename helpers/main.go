b6ddbc60c1b3a8ab1d411f882b111fd6bf7ba0c4
AmiraliAshoori <rouchashoori@gmail.com>
2024-12-17 17:20:31 +0330

    added different input types

--- helpers/main.go
+++ helpers/main.go
@@ -1,15 +1,58 @@
 package helpers
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 
 
-func getUserInput(message string) string {
+func getTextInput(message string) string {
 	var input string
 
 	fmt.Println(message)
 	fmt.Scanln(&input)
 
+	if input == "" {
+		fmt.Println("input can not be empty")
+		getTextInput(message)
+	}
 
 	return input
+}
+
+
+func getNumberInput(message string) uint {
+	var input uint
+
+	if input == 0 {
+		fmt.Println("input can not be equal or less than 0")
+		getNumberInput(message)
+	}
+
+	fmt.Println(message)
+	fmt.Scanln(&input)
+
+	return input
+}
+
+
+func getYesNoInput(message string) bool {
+	var input string
+
+
+	fmt.Println(message)
+	fmt.Scanln(&input)
+
+
+	input = strings.ToLower(input)
+
+	if input == "y"{
+		return true
+	} else if input == "n" {
+		return false
+	} else {
+		val := getYesNoInput(message)
+		return val
+	}
 }
